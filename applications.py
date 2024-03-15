eb87dd11af5f4d6059cc22f1f6cc125edec3c4d5
mohaned0002 <mohamedmohaned633@gmail.com>
2024-03-16 00:49:55 +0200

    part2 edit_kth_largest

--- applications.py
+++ applications.py
@@ -31,14 +31,38 @@ def insertion_sort_hybrid(arr, l, h):
 
 arr = sorting.generate_arr(1000)
 threshold = 5
-print(arr)
+
 sorted_arr = hybrid_sort(arr, threshold)
 print("Sorted array:", sorted_arr)
+
+
+
+#################
+
+import random
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    leftPointer = low
+    rightPointer = high - 1
+    while leftPointer <= rightPointer:
+        while leftPointer <= rightPointer and arr[leftPointer] < pivot:
+            leftPointer += 1
+        while leftPointer <= rightPointer and arr[rightPointer] > pivot:
+            rightPointer -= 1
+        if leftPointer <= rightPointer:
+            arr[leftPointer], arr[rightPointer] = arr[rightPointer], arr[leftPointer]
+            leftPointer += 1
+            rightPointer -= 1
+
+    arr[leftPointer], arr[high] = arr[high], arr[leftPointer]
+    return leftPointer
+
 def find_kth_largest(arr, k):
     low = 0
     high = len(arr) - 1
     while low <= high:
-        pivot_index = sorting.partition(arr, low, high,arr[high])
+        pivot_index = partition(arr, low, high)
         if pivot_index == len(arr) - k:
             return arr[pivot_index]
         elif pivot_index < len(arr) - k:
@@ -47,8 +71,9 @@ def find_kth_largest(arr, k):
             high = pivot_index - 1
     return None
 
-arr = [3, 55, 16, 25, 63, 52, 40]
+arr = [random.randint(0, 3000) for _ in range(300)]
+print(arr)
 k = 3
-print("Kth largest element is:", find_kth_largest(arr, k))
+print("Output:", find_kth_largest(arr, k))
 
 
