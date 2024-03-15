3a9c84a9b87ce75c6d75f4103d9d61550856bff2
Adham <adhamm.khaled7@gmail.com>
2024-03-15 21:05:24 +0200

    testing algorithms

--- sorting.py
+++ sorting.py
@@ -1,4 +1,25 @@
 import random
+import time
+import copy
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_ind = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_ind]:
+                min_ind = j
+        arr[i], arr[min_ind] = arr[min_ind], arr[i]
+
 
 def mergeSort(arr):
     _mergeSort(arr,0,len(arr)-1)
@@ -78,23 +99,66 @@ def partition(arr, high, low, pivot):
             leftPointer += 1
         while (arr[rightPointer] > pivot and leftPointer < rightPointer):
             rightPointer -= 1
-        arr[rightPointer], arr[leftPointer] = arr[leftPointer], arr[rightPointer]
+        if(leftPointer<=rightPointer):
+            arr[rightPointer], arr[leftPointer] = arr[leftPointer], arr[rightPointer]
+            leftPointer += 1
+            rightPointer -= 1
 
     if(arr[leftPointer]>arr[high]):
         arr[leftPointer], arr[high] = arr[high], arr[leftPointer]
     else:
         leftPointer=high
     return leftPointer
+def generate_arr(x):
+    arr=[]
+    for i in range(x):
+        arr.append(random.randint(0,x))
+    return arr
+
+def test_algorithms (x,nsquared):
+    arr1 = generate_arr(x)
+    if nsquared is True :
+        arr2 = copy.deepcopy(arr1)
+    arr3 = copy.deepcopy(arr1)
+    arr4 = copy.deepcopy(arr1)
+    arr5 = copy.deepcopy(arr1)
+    if nsquared is True:
+        time_start = time.time()
+        insertion_sort(arr1)
+        time_end = time.time()
+        running_time = (time_end - time_start)*1000
+        print(f'Running time for insertion sort for size {x} is {running_time} ms')
+
+        time_start = time.time()
+        selection_sort(arr2)
+        time_end = time.time()
+        running_time = (time_end - time_start) * 1000
+        print(f'Running time for selection sort for size {x} is {running_time} ms')
+
+    time_start = time.time()
+    mergeSort(arr3)
+    time_end = time.time()
+    running_time = (time_end - time_start) * 1000
+    print(f'Running time for merge sort for size {x} is {running_time} ms')
+
+    time_start = time.time()
+    heapSort(arr4)
+    time_end = time.time()
+    running_time = (time_end - time_start) * 1000
+    print(f'Running time for heap sort for size {x} is {running_time} ms')
+
+    time_start = time.time()
+    quickSort(arr5)
+    time_end = time.time()
+    running_time = (time_end - time_start) * 1000
+    print(f'Running time for quick sort for size {x} is {running_time} ms')
+
+test_algorithms(1000,True)
+test_algorithms(25000,True)
+test_algorithms(50000,True)
+test_algorithms(100000,True)
+test_algorithms(1000000,False)
 
 
-arr = [0,583,82,573,2,3,9821,22]
-mergeSort(arr)
-print(arr)
-arr = [0,583,82,573,2,3,9821,22]
-heapSort(arr)
-print(arr)
-arr = [0,583,82,573,2,3,9821,22,7]
-quickSort(arr)
-print(arr)
 
 
